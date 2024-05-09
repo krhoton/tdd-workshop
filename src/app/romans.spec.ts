9d5e51eaccde6628c4c213c4a5b3a904f108d36d
Kenan Rhoton <krhoton@redsauce.net>
2024-05-09 11:47:30 +0200

    Romans solution

--- src/app/romans.spec.ts
+++ src/app/romans.spec.ts
@@ -22,4 +22,37 @@ describe("Roman numerals", () => {
     /*ASSERT**/
     expect(value).toBe("III");
   });
+  
+  it("Should convert '9' to 'IX'", () => {
+    /*ARRANGE**/
+    const numerals: Romans = new Romans();
+    
+    /*ACT**/
+    const value: string = numerals.convert(9);
+    
+    /*ASSERT**/
+    expect(value).toBe("IX");
+  });
+
+  it("Should convert '1066' to 'MLXVI'", () => {
+    /*ARRANGE**/
+    const numerals: Romans = new Romans();
+    
+    /*ACT**/
+    const value: string = numerals.convert(1066);
+    
+    /*ASSERT**/
+    expect(value).toBe("MLXVI");
+  });
+
+  it("Should convert '1989' to 'MCMLXXXIX'", () => {
+    /*ARRANGE**/
+    const numerals: Romans = new Romans();
+    
+    /*ACT**/
+    const value: string = numerals.convert(1989);
+    
+    /*ASSERT**/
+    expect(value).toBe("MCMLXXXIX");
+  });
 });
--- src/app/romans.ts
+++ src/app/romans.ts
@@ -1,10 +1,34 @@
+const ROMAN_SYMBOLS: Array<[string, string, string]> = [
+  [".", ".", "."],
+  ["I", "V", "X"],
+  ["X", "L", "C"],
+  ["C", "D", "M"],
+  ["M", "?", "?"],
+]
+
 export class Romans {
 
-  convert(num: number): string {
-    let numeral: string = "";
+  convert_digit(num: number, ones: string, fives: string, tens: string): string {
+    let result: string = "";
+    if (num === 9) return ones+tens;
+    if (num > 5) {
+      result += fives;
+      num -= 5;
+    };
+    if (num === 4) return ones+fives;
     for (let i: number = 0; i < num; i++) {
-      numeral += 'I';
+      result += ones;
     }
-    return numeral;
+    return result;
+  }
+
+  convert(num: number): string {
+    let result: string = "";
+    const stringNum: string = num.toString();
+    let size: number = stringNum.length;
+    for (let i: number = 0; i < size; i++) {
+      result += this.convert_digit(parseInt(stringNum[i]), ...ROMAN_SYMBOLS[size-i])
+    }
+    return result;
   }
 }
