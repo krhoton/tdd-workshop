398ef2c82c01105e0e5ade032cfa59538c98b489
sergio.macias <sergio.macias@vincle.com>
2024-05-13 12:10:13 +0200

    template and test updated

--- src/app/components/numerals/numerals.component.spec.ts
+++ src/app/components/numerals/numerals.component.spec.ts
@@ -11,13 +11,27 @@ describe('NumeralsComponent', () => {
       imports: [NumeralsComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(NumeralsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should successfully create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should successfully convert 1 to I', () => {
+    const component = fixture.debugElement.componentInstance;
+    component.parsedValue$.subscribe(
+      (data:string) => {
+        expect(data).toBe('I')
+      }
+    );
+
+    const romanInput = fixture.nativeElement.querySelector('input')
+    romanInput.value = '1';
+    romanInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
   });
 });
